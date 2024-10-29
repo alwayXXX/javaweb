2691d1621bee6890f43acb64c9a058fd752c1bac
alwayXXX <69452135+alwayXXX@users.noreply.github.com>
2024-10-29 16:53:24 +0800

    Add files via upload

---
+++ java3/src/main/java/org/example/java3/cookie/CookieDemo2.java
@@ -0,0 +1,25 @@
+package org.example.java3.cookie;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/cookieDemo2")
+public class CookieDemo2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie[] cookies = req.getCookies();
+        for (Cookie cookie : cookies) {
+            String name = cookie.getName();
+            if ("msg".equals(name)){
+                System.out.println(cookie.getValue());
+                break;
+            }
+        }
+    }
+}
